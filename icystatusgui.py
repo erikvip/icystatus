30991a571b75485920638bd05de3e8a116e26663
Erik Phillips <erikvip@gmail.com>
2015-01-02 17:45:06 -0700

    GUI updates, added status bar

--- icystatusgui.py
+++ icystatusgui.py
@@ -4,6 +4,8 @@ Displays shoutcast streams in a nice GUI
 
 """
 from Tkinter import *
+import ttk
+
 from icystatus import fetchStatus
 import pprint
 
@@ -21,28 +23,54 @@ class icyStatusGui:
         self.root.title("icyStatus - Currently Playing")
         #self.root.maxsize(800,600)
 
-#        self.win = Label(self.root, text="Hello", width=200, height=75) 
- #       self.win.pack()
+        '''
+        style = ttk.Style()
+        style.configure("BW.TLabel", foreground="black", background="white")        
+        self.menubar = Menu(self.root)
+        self.filemenu = Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="Quit", command=self.root.quit)
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
+        '''
 
+        
+        toolbar = Frame(self.root, bd=1, relief=RAISED)
+        refreshButton = Button(toolbar, text="Refresh", relief=FLAT, command=self.icyStatus).pack(side=LEFT)
+        toolbar.pack(side=TOP, fill=X)
+
+        self.grid = Frame(self.root, bd=1)
+        self.grid.pack(fill=BOTH, expand=1)
+
+
+        self.status = StringVar()
+        self.statusFrame = Frame(self.root, bd=1, relief=RAISED)
+        self.statusLabel = Label(self.statusFrame, textvariable=self.status, anchor=W, justify=LEFT).pack(fill=X, expand=1)
+        self.statusFrame.pack(side=BOTTOM, fill=X)
 
 
 
         # Main listbox 
-        #self.listbox = Listbox(self.root)
-        #self.listbox.pack()
-        Label(self.root, text="Station", width = 10).grid(row = 0, column = 0)
-        Label(self.root, text="URL", width = 10).grid(row = 0, column = 1)
-        Label(self.root, text="Current Track", width = 30).grid(row = 0, column = 2)
+        Label(self.grid, text="Station", width = 10).grid(row = 0, column = 0)
+        Label(self.grid, text="URL", width = 10).grid(row = 0, column = 1)
+        Label(self.grid, text="Current Track", width = 30).grid(row = 0, column = 2)
 
         self.icyStatus()
+
+        
+
+
+        self.root.geometry("800x450+300+300")
+        #self.root.config(menu=self.menubar)
 
         self.root.mainloop()
 
         
 
     def icyStatus(self):
+        
+        self.status.set("Refreshing...")
 
-
+        # SomaFM URLs
+        '''
         urls = [
             'http://xstream1.somafm.com:8062',
             'http://xstream1.somafm.com:2800',
@@ -58,19 +86,25 @@ class icyStatusGui:
             'http://xstream1.somafm.com:8884',
             'http://uwstream2.somafm.com:7770',
         ]
+        '''
+        urls = [
+        'http://205.164.62.15:6900',
+        'http://108.61.73.115:8052',
+        #'http://listen.radionomy.com/AdultAlternative'
+        ]
 
         stats = fetchStatus(urls)
-        pprint.pprint(stats)
+      
 
         r = 1
         for i in stats:
-            Label(self.root, text=i['Name'][:20]).grid(row = r, column=0)
-            Label(self.root, text=i['StreamUrl']).grid(row = r, column=1)
-            Label(self.root, text=i['CurrentTrack']).grid(row = r, column=2)
+            Label(self.grid, text=i['Name'][:20], bg="#EEEEEE", anchor=W, justify=LEFT).grid(row = r, column=0)
+            Label(self.grid, text=i['StreamUrl'][:30]).grid(row = r, column=1)
+            Label(self.grid, text=i['CurrentTrack']).grid(row = r, column=2)
 
             r = r + 1;
 
-
+        self.status.set("Refresh complete")
 
 
 
